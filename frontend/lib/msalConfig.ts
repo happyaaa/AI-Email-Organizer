15cced1dad8e5582023482c063aea8a530ffed08
Xing Gao <xinggao6@illinois.edu>
2025-04-30 10:09:50 -0500

    change clientID allow all user login

--- frontend/lib/msalConfig.ts
+++ frontend/lib/msalConfig.ts
@@ -3,9 +3,15 @@ import { PublicClientApplication } from "@azure/msal-browser";
 export const getMsalInstance = () => {
   const msalConfig = {
     auth: {
-      clientId: "990298d9-b3c0-4096-b31c-15c19aa209bb",
-      authority: "https://login.microsoftonline.com/44467e6f-462c-4ea2-823f-7800de5434e3",
-      redirectUri: typeof window !== "undefined" ? window.location.origin : "", // prevent SSR crash
+      clientId: "44b4a98b-3144-48f8-bc1e-5413975993e1",
+      authority: "https://login.microsoftonline.com/common",  
+      redirectUri: typeof window !== "undefined" ? window.location.origin : "",
+      navigateToLoginRequestUrl: true,
+      postLogoutRedirectUri: typeof window !== "undefined" ? window.location.origin : "",
+    },
+    cache: {
+      cacheLocation: "sessionStorage",
+      storeAuthStateInCookie: true
     },
   };
 
@@ -14,4 +20,5 @@ export const getMsalInstance = () => {
 
 export const loginRequest = {
   scopes: ["Mail.Read", "Mail.ReadWrite", "Mail.Send", "User.Read"],
+  prompt: "select_account", 
 };
