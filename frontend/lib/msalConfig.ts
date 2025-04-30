17680c0a6b36514118530fd11ff4f98bf74b568c
timy-0128 <ty42@illinois.edu>
2025-04-30 01:48:09 -0500

    config change

--- frontend/lib/msalConfig.ts
+++ frontend/lib/msalConfig.ts
@@ -1,17 +1,9 @@
 import { PublicClientApplication } from "@azure/msal-browser";
 
-const msalConfig = {
-  auth: {
-    clientId: "44b4a98b-3144-48f8-bc1e-5413975993e1",
-    authority: "https://login.microsoftonline.com/44467e6f-462c-4ea2-823f-7800de5434e3",
-    redirectUri: typeof window !== "undefined" ? window.location.origin : "",
-  },
-};
-
 export const getMsalInstance = () => {
   const msalConfig = {
     auth: {
-      clientId: "44b4a98b-3144-48f8-bc1e-5413975993e1",
+      clientId: "990298d9-b3c0-4096-b31c-15c19aa209bb",
       authority: "https://login.microsoftonline.com/44467e6f-462c-4ea2-823f-7800de5434e3",
       redirectUri: typeof window !== "undefined" ? window.location.origin : "", // prevent SSR crash
     },
